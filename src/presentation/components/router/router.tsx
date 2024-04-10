8d956b79e291bc40e2e42109924bb050d4735b50
Pedro Luan <pedro.dias@olxbr.com>
2024-04-10 13:43:18 -0300

    chore: back to router v5

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { Login } from '@/presentation/pages'
 import { AuthenticationSpy, ValidationStub } from '@/presentation/test'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 const Router: React.FC = () => {
   const validationStub = new ValidationStub()
   const authenticationSpy = new AuthenticationSpy()
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={
+      <Switch>
+        <Route path="/login" render={() => (
           <Login
             validation={validationStub}
             authentication={authenticationSpy}
           />
-        } />
-      </Routes>
+        )} />
+      </Switch>
     </BrowserRouter>
   )
 }
