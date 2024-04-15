d682ddb8e2ba24c339c639dbf8d2b6af21b04605
Pedro Luan <pedro.dias@olxbr.com>
2024-04-15 09:28:31 -0300

    refactor: extract sut creation

--- src/validation/validation-composite/validation-composite.spec.ts
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -1,17 +1,28 @@
-import { FieldValidation } from "../protocols/field-validation"
-import { FieldValidationSpy } from "../validators/test/mock-field-validation";
+import { FieldValidationSpy } from './../validators/test/mock-field-validation';
 import { ValidationComposite } from "./validation-composite";
+
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationSpyList: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const fieldValidationSpyList = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field')
+  ]
+  const sut = new ValidationComposite(fieldValidationSpyList)
+
+  return {
+    sut, fieldValidationSpyList
+  }
+}
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field')
-    fieldValidationSpy.error = new Error("first_error_message")
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error("any_error_message")
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2
-    ])
+    const { sut, fieldValidationSpyList } = makeSut()
+    fieldValidationSpyList[0].error = new Error("first_error_message")
+    fieldValidationSpyList[1].error = new Error("any_error_message")
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe("first_error_message")
   })
