f4bf139347c36c8dd6be55bf30f011dc7134505d
Pedro Luan <pedro.dias@olxbr.com>
2024-04-16 15:01:56 -0300

    refactor: extract sut creation

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -3,13 +3,17 @@ import { render, screen } from '@testing-library/react'
 import Input from './input'
 import Context from '@/presentation/contexts/form/form-context'
 
+const makeSut = (): void => {
+  render(
+    <Context.Provider value={{ state: {} }}>
+      <Input name="test-input" />
+    </Context.Provider >
+  )
+}
+
 describe('Input component', () => {
   test('Should begin with readonly', () => {
-    render(
-      <Context.Provider value={{ state: {} }}>
-        <Input name="test-input" />
-      </Context.Provider >
-    )
+    makeSut()
     const input = screen.getByTestId('test-input') as HTMLInputElement
     expect(input.readOnly).toBe(true)
   })
