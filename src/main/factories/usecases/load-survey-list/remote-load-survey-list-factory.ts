25dd69dd5fb7395be55669251fddba18eba39804
Pedro Luan <pedro.dias@olxbr.com>
2024-08-13 18:07:30 -0300

    feat: add makeAuthorizeHttpGetClientDecorator factory

---
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -0,0 +1,8 @@
+import { RemoteLoadSurveyList } from "@/data/usecases/load-survey-list/remote-load-survey-list";
+import { LoadSurveyList } from "@/domain/usecases";
+import { makeApiUrl } from "@/main/factories/http";
+import { makeAuthorizeHttpGetClientDecorator } from "@/main/factories/decorators";
+
+export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
+  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAuthorizeHttpGetClientDecorator())
+}
