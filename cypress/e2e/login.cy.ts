4dbbcd292c5f2d5a973c83d2ceea551423b31375
Pedro Luan <pedro.dias@olxbr.com>
2024-10-31 07:50:51 -0300

    feat: add cypress getByTestId command

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,9 @@
 describe('template spec', () => {
-  it('Should load with correct values', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('Should load with correct values', () => {
+    cy.getByTestId('email-status').should('have.attr','title','Campo obrigatório')
   })
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,7 @@
 /// <reference types="cypress" />
+
+Cypress.Commands.add('getByTestId',(id) => cy.get(`[data-testid=${id}]`))
+
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
