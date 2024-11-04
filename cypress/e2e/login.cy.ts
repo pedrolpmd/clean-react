1e97caf670829ac374ba3d81baa2bd95d88cd3fc
Pedro Luan <pedro.dias@olxbr.com>
2024-11-04 17:09:44 -0300

    test: ensure Login save access token

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,7 @@
 import faker from 'faker'
 
+const baseUrl: string | null = Cypress.config().baseUrl
+
 describe('template spec', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -49,4 +51,33 @@ describe('template spec', () => {
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner').should('exist')
+      .getByTestId('main-error').should('not.exist')
+      .getByTestId('spinner').should('not.exist')
+      .getByTestId('main-error').should('exist')
+    
+    cy.url().should('eq', `${baseUrl}/login`)
+  })
+
+/*   it('Should save accessToken if valid credentials are provided', () => {
+    cy.getByTestId('email').focus().type('mango@gmail.com')
+    cy.getByTestId('password').focus().type('12345')
+    cy.getByTestId('submit').click()
+
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner').should('exist')
+      .getByTestId('main-error').should('not.exist')
+      .getByTestId('spinner').should('not.exist')
+    
+    cy.url().should('eq', `${baseUrl}/`)
+
+    cy.window().then(window => assert.isOk(window.localStorage.getItem('acessToken')))
+  }) */
 })
