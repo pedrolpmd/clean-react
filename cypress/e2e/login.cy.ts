d1ce919934882e935523fbe72c1698900c0d9292
Pedro Luan <pedro.dias@olxbr.com>
2024-11-14 07:53:25 -0300

    refactor: change test to simulate submit with enter key

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -84,8 +84,7 @@ describe('template spec', () => {
     })
 
     cy.getByTestId('email').focus().type(faker.internet.email())
-    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
-    cy.getByTestId('submit').click()
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5)).type('{enter}')
 
     cy.getByTestId('error-wrap')
       .getByTestId('spinner').should('not.exist')
