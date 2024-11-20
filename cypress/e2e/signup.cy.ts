132ec48863871c726cef63513eb5abb6a38def7d
Pedro Luan <pedro.dias@olxbr.com>
2024-11-20 08:19:34 -0300

    test: ensure signup presents error state

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,5 +1,6 @@
 import { testInputStatus } from "../support/form-helper"
 import { mockOptionsRequest } from "../support/http-mocks"
+import faker from 'faker'
 
 describe('login', () => {
 
@@ -20,6 +21,24 @@ describe('login', () => {
 
     cy.getByTestId('passwordConfirmation').should('have.attr', 'readOnly')
     testInputStatus('passwordConfirmation', 'Campo obrigatório')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+
+    cy.getByTestId('name').focus().type(faker.random.alphaNumeric(3))
+    testInputStatus('name', 'Valor inválido')
+
+    cy.getByTestId('email').focus().type(faker.random.word())
+    testInputStatus('email', 'Valor inválido')
+
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    testInputStatus('password', 'Valor inválido')
+
+    cy.getByTestId('passwordConfirmation').focus().type(faker.random.alphaNumeric(4))
+    testInputStatus('passwordConfirmation', 'Valor inválido')
 
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
