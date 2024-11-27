07ddf59456c6c44a246898a1b6968b24ab3e0de3
Pedro Luan <pedro.dias@olxbr.com>
2024-11-27 09:10:10 -0300

    feat: ensure remoteLoadSurveyResult throws UnexpectedError on 500

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -44,4 +44,14 @@ describe('RemoteLoadSurveyList', () => {
     const promise = sut.load()
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('Should throw UnexpectedError if HttpGetClient returns 500', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
