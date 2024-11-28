b19d49de0c15f811ee066cf1dfe0fb39aaccdb0d
Pedro Luan <pedro.dias@olxbr.com>
2024-11-28 09:30:33 -0300

    feat: add skeleton to survey result

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -11,29 +11,23 @@ const SurveyList: React.FC = ({ }) => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <hgroup>
-          <Calendar date={new Date()} className={Styles.calendarWrap}/>
-          <h2>Qual é seu framework favorito?</h2>
-        </hgroup>
-        <FlipMove className={Styles.answearList}>
-          <li>
-            <img src='https://delta-dev-software.fr/wp-content/uploads/2024/02/react-logo-freelogovectors.net_.png' />
-            <span className={Styles.answear}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li className={Styles.active}>
-            <img src='https://delta-dev-software.fr/wp-content/uploads/2024/02/react-logo-freelogovectors.net_.png' />
-            <span className={Styles.answear}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li>
-            <img src='https://delta-dev-software.fr/wp-content/uploads/2024/02/react-logo-freelogovectors.net_.png' />
-            <span className={Styles.answear}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-        </FlipMove>
-        <button>Voltar</button>
-        <Loading />
+        {true &&
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap} />
+              <h2>Qual é seu framework favorito?</h2>
+            </hgroup>
+            <FlipMove className={Styles.answearList}>
+              <li>
+                <img src='https://delta-dev-software.fr/wp-content/uploads/2024/02/react-logo-freelogovectors.net_.png' />
+                <span className={Styles.answear}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+            </FlipMove>
+            <button>Voltar</button>
+            <Loading />
+          </>
+        }
       </div>
       <Footer />
     </div>
