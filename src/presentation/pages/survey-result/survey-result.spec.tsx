77b262e55aa3ca68074d687d88c38eb8261e8a70
Pedro Luan <pedro.dias@olxbr.com>
2024-11-29 10:22:20 -0300

    refactor: extract sut creation to a factory method

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -5,15 +5,19 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from "@testing-library/react"
 import React from "react"
 
+const makeSut = (): void => {
+  render(
+    <ApiContext.Provider value={{
+      setCurrentAccount: jest.fn(),
+      getCurrentAccount: () => mockAccountModel()
+    }}>  <SurveyResult />
+    </ApiContext.Provider>
+
+  )
+}
 describe('SurveyResult component', () => {
   test('Should present correct initial state', async () => {
-    render(
-    <ApiContext.Provider value={{
-        setCurrentAccount: jest.fn(),
-        getCurrentAccount: () => mockAccountModel()
-      }}>  <SurveyResult /> </ApiContext.Provider>
-     
-   )
+    makeSut()
     const surveyResult = screen.getByTestId('survey-result')
     expect(surveyResult.childElementCount).toBe(0)
     expect(screen.queryByTestId('error')).not.toBeInTheDocument()
